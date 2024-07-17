cc10e22fa9fb171fba03fc029d7b07d0ee01b16f
Parvez Ahmed <miazyparvez@gmail.com>
2024-07-17 12:44:54 +0600

    templating with signup done

--- queries/courses.js
+++ queries/courses.js
@@ -1,22 +1,102 @@
-import { Course } from "@/model/course-model";
 import { Category } from "@/model/category-model";
-import { User } from "@/model/user-model";
-import { Testimonial } from "@/model/testimonial-model";
+import { Course } from "@/model/course-model";
 import { Module } from "@/model/module.model";
+import { Testimonial } from "@/model/testimonial-model";
+import { User } from "@/model/user-model";
 
-export async function getCourses() {
-    const courses = await Course.find({}).populate({
-        path: "category",
-        model: Category
-    }).populate({
-        path: "instructor",
-        model: User
-    }).populate({
-        path: "testimonials",
-        model: Testimonial
-    }).populate({
-        path: "modules",
-        model: Module
-    });
-    return courses;
+import {
+  replaceMongoIdInArray,
+  replaceMongoIdInObject,
+} from "@/lib/convertData";
+import { getEnrollmentsForCourse } from "./enrollments";
+import { getTestimonialsForCourse } from "./testimonials";
+
+export async function getCourseList() {
+  const courses = await Course.find({})
+    .select([
+      "title",
+      "subtitle",
+      "thumbnail",
+      "modules",
+      "price",
+      "category",
+      "instructor",
+    ])
+    .populate({
+      path: "category",
+      model: Category,
+    })
+    .populate({
+      path: "instructor",
+      model: User,
+    })
+    .populate({
+      path: "testimonials",
+      model: Testimonial,
+    })
+    .populate({
+      path: "modules",
+      model: Module,
+    })
+    .lean();
+  return replaceMongoIdInArray(courses);
+}
+export async function getCourseDetails(id) {
+  const course = await Course.findById(id)
+    .populate({
+      path: "category",
+      model: Category,
+    })
+    .populate({
+      path: "instructor",
+      model: User,
+    })
+    .populate({
+      path: "testimonials",
+      model: Testimonial,
+      populate: {
+        path: "user",
+        model: User,
+      },
+    })
+    .populate({
+      path: "modules",
+      model: Module,
+    })
+    .lean();
+  return replaceMongoIdInObject(course);
+}
+
+export async function getCourseDetailsByInstructor(instructorId) {
+  const courses = await Course.find({ instructor: instructorId }).lean();
+  const enrollments = await Promise.all(
+    courses.map(async (course) => {
+      const enrollment = await getEnrollmentsForCourse(course._id.toString());
+      return enrollment;
+    })
+  );
+  const totalEnrollments = enrollments.reduce((item, currentValue) => {
+    return item.length + currentValue.length;
+  });
+
+  const testimonials = await Promise.all(
+    courses.map(async (course) => {
+      const testimonial = await getTestimonialsForCourse(course._id.toString());
+      return testimonial;
+    })
+  );
+
+  const totalTestimonials = testimonials.flat();
+
+  const avgRating =
+    totalTestimonials.reduce(function (acc, obj) {
+      return acc + obj.rating;
+    }, 0) / totalTestimonials.length;
+
+  return {
+    courses: courses.length,
+    enrollments: totalEnrollments,
+    reviews: totalTestimonials,
+    ratings: avgRating.toPrecision(2),
+  };
 }
--- service/mongo.js
+++ service/mongo.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-export async function dbConnect(){
-    try {
-        const conn = await mongoose.connect(String(process.env.MONGODB_CONNECTION_STRING));
-        return conn;
-    } catch(err) {
-        console.error(err);
-    }
+export async function dbConnect() {
+  try {
+    const conn = await mongoose.connect(
+      String(process.env.MONGODB_CONNECTION_STRING)
+    );
+    return conn;
+    console.log(conn);
+  } catch (err) {
+    console.error(err);
+  }
 }
